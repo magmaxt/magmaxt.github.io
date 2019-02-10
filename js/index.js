4bd471663b0d261cb91170af403b786cc333d5c6
Tian_mac_book <x.tian@columbia.edu>
2019-02-09 21:22:12 -0500

    some test based on learning from https://learn.devfe.st/beginner

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('code').on('mouseover', function() {
+  $(this).html('老婆');
+});
+$('code').on('click', function() {
+  $(this).html('你好美！');
+});
+$('code').on('dblclick', function() {
+  $(this).html('我好想你！');
+});
+$('code').on('keydown', function() {
+  $(this).html('昨天晚上睡得好吗？');
+});
