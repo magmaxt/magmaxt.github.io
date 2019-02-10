5538c8401edcf8234d94c30359980a8d781552fd
Tian_mac_book <x.tian@columbia.edu>
2019-02-09 22:11:24 -0500

    testing animation in index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ $('code').on('dblclick', function() {
 $('code').on('keydown', function() {
   $(this).html('昨天晚上睡得好吗？');
 });
+
+$('p').on('click', function() {
+  $(this).css({
+    "color": "#42cbad",
+    "font-weight": "700",
+    "line-height": "40px",
+  });
+});
+/* I included this in my CSS so that you could run the snippet below! */
+
+$('p').on('mouseover', function() {
+  $(this).toggleClass('extra-cool-text');
+});
+
+// $('h1').on('mouseover', function() {
+//   $(this).animate({
+//     "letter-spacing": "10px",
+//     "font-size": "40px"
+//   }, 1000);
+// });
+// $('h1').on('click', function() {
+//   $(this).animate({ "font-size" : "40px" }, 400)
+//   .animate({ "letter-spacing" : "10px" }, 5000);
+// });
+
+// Run this in the console, then click me
+$('h1').on('click', function() {
+  $(this).animate({
+    "height" : "0px",
+    "padding": "0px"
+  }, 400, function() {
+    $(this).html("");
+    alert("goodbye world"); // like console.log() but better
+  })
+});
